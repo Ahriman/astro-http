274c36e2a5978ddab6e6c2d328526e6f5fb8bd6b
Marcos Alberto Sanmartín Pereira <ahriman89@gmail.com>
2024-09-14 01:41:47 +0200

    another config for cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,11 +11,19 @@ import db from "@astrojs/db";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [
-    mdx(),
-    sitemap(),
-    process.env.NODE_ENV === 'production' ? null : db()
-  ].filter(Boolean),
+  integrations: [mdx(), sitemap(), db()],
   output: "hybrid",
   adapter: cloudflare(),
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ['astro:db'],
+        output: {
+          globals: {
+            'astro:db': 'AstroDB'
+          }
+        }
+      }
+    }
+  }
 });
